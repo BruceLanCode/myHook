b61f258d4ea38581f31d5695415865737049c0e9
lantuhui <shengxiaobai15@163.com>
2017-04-02 19:25:25 +0800

    lantu_0402_19:22

---
+++ src/script/config/router.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('app',['ui.router']).config(['$stateProvider','$urlRouterProvider',
+	function($stateProvider,$urlRouterProvider) {
+	$stateProvider.state('main',{
+		url:'/main',
+		templateUrl:'view/main.html',
+		controller:'mainCtrl'
+	});
+	$urlRouterProvider.otherwise('/main');
+}])
